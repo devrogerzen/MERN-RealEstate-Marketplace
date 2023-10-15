88263f92bccaf21586cdd06d7f7d87b8b479a17d
devrogerzen <devrogerzen@gmail.com>
2023-10-15 00:41:02 -0300

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js"
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -14,8 +15,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
 app.use("/api/user", userRouter)
+app.use("/api/auth", authRouter)
