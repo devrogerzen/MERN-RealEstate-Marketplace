842c36d891d5c7a8ea5035401256f080863c2cf8
devrogerzen <devrogerzen@gmail.com>
2023-10-16 20:09:17 -0300

    complete sign in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -39,7 +39,7 @@ export default function SignUp() {
   };
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold ">SignUp</h1>
+      <h1 className="text-3xl text-center font-semibold my-7 ">Sign Up</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
@@ -66,15 +66,13 @@ export default function SignUp() {
           disabled={loading}
           className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 "
         >
-          Sign up
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
-        <Link>
-          <span className="text-blue-700">
-            {loading ? "Loading..." : "Sign in"}
-          </span>
+        <Link to={'/sign-in'}>
+          <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
       {error && <p className="text-red-500 mt-5">{error}</p>}
